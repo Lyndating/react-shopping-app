4ddab677549bcafd15dbb975291096e29db90a10
Ting Luo <shirleyayu@hotmail.com>
2022-05-25 15:31:07 +1000

    add footer to pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {auth} from './firebase';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import ProductShow from './components/ProductShow';
 import Checkout from './components/Checkout';
 import Login from './components/Login';
+import Payment from './components/Payment';
 import {useStateValue} from './helper/StateProvider';
 
 
@@ -35,10 +37,11 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-          <Route exact path="/" element={[<Header/>,<Home/>]}/>
-          <Route path="/products/:id" element={[<Header/>,<ProductShow/>]}/>
-          <Route path="/checkout" element={[<Header/>,<Checkout/>]}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route exact path="/" element={[<Header/>,<Home/>,<Footer/>]}/>
+          <Route path="/products/:id" element={[<Header/>,<ProductShow/>,<Footer/>]}/>
+          <Route path="/checkout" element={[<Header/>,<Checkout/>,<Footer/>]}/>
+          <Route path="/payment" element={[<Header/>,<Payment/>,<Footer/>]}/>
+          <Route path="/login" element={[<Login/>,<Footer/>]}/>
         </Routes>
       </div>
     </Router>
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='footer_info'>
+
+        </div>
+        <div className='footer_signup'>
+            <h5>SIGN UP NOW</h5>
+            <p> for the latest new and trends</p>
+            <button> Creates an Account </button>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
