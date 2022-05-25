20a83aba2450522c88771b1708d4c5047b204bd9
Ting Luo <shirleyayu@hotmail.com>
2022-05-25 15:34:58 +1000

    change product card style

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,14 +3,16 @@ import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { useStateValue } from '../helper/StateProvider';
+import {Link} from 'react-router-dom';
 
-function Product({id, title, image, price, rating}) {
-    const [state, dispatch] = useStateValue();
+function Product({id,brand, title, image, price, rating}) {
+    const [{basket}, dispatch] = useStateValue();
     const addToCart = () => {
         dispatch({
-            type: 'ADD_TO_CART',
+            type: 'add_to_basket',
             item: {
                 id: id,
+                brand: brand,
                 title: title,
                 image: image,
                 price: price,
@@ -18,22 +20,27 @@ function Product({id, title, image, price, rating}) {
             },
         });
     }
+
   return (
     <div className='product'>
+        <Link to={`/products/${id}`} >
+        <img 
+            src={image} alt=''/>
+        </Link>
         <div className='product_info'>
+            <p><strong>{brand}</strong></p>
             <p>{title}</p> 
             <p className='product_price'>
                 <small>$</small>
                 <strong>{price}</strong>
             </p> 
             <div className='product_rating'>
-                {Array(rating).fill().map((_, i)=>(<p><StarIcon/></p>))}
+                {Array(rating).fill().map((n, i)=>(<p><StarIcon/></p>))}
 
            </div>
         </div>
-        <img 
-            src={image} alt=''/>
-            <button onClick={addToCart}><AddShoppingCartIcon/></button>
+
+            <button onClick={addToCart}>Add Item</button>
     </div>
   )
 }
