261ad7985334c5d82bddae14565cac8b48234b0a
Ting Luo <shirleyayu@hotmail.com>
2022-05-27 15:40:20 +1000

    add signup link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import { useNavigate } from 'react-router-dom';
 const Footer = () => {
+    const navigate=useNavigate();
   return (
     <div className='footer'>
         <div className='footer_info'>
@@ -9,7 +11,7 @@ const Footer = () => {
         <div className='footer_signup'>
             <h5>SIGN UP NOW</h5>
             <p> for the latest new and trends</p>
-            <button> Creates an Account </button>
+            <button onClick={()=>{navigate("/login")}}> Creates an Account </button>
         </div>
     </div>
   )
