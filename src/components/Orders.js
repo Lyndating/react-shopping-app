3c3f59ece221fa82bf704759e614ee67e65b3fe6
Ting Luo <shirleyayu@hotmail.com>
2022-05-27 22:59:44 +1000

    set up Order component

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'; 
+import { useNavigate } from 'react-router-dom';
+import {db} from '../firebase-config';
+import { useStateValue } from '../helper/StateProvider';
+import './Order.css';
 
 const Orders = () => {
+    const [{basket, address, user}, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+    //fetch order data from db
+    useEffect(()=>{
+        if(user){
+        db.collection('users').doc(user?.uid).collection('orders').orderBy('created','desc').onSnapshot(snapshot => {
+            console.log(snapshot.docs);
+            setOrders(snapshot.docs.map(doc=>({
+                id: doc.uid,
+                data: doc.data()
+            })))
+        })}else {
+            setOrders([]);
+        }
+    },[user]);
+
+    console.log(orders);
+
+    //empty order list redirect to home page 
+    const routeChange = () => {
+        navigate('/');
+    };
+
+
   return (
-    <div> Your Order History</div>
+      <div className='order_history_container'>
+        <div className='order_history_title'>
+            <h2>Your Orders</h2>
+        </div>
+        {orders.length=== 0 && 
+            <div className='empty_order_container'>
+                <h3>
+                    There is no Order History.
+                </h3>
+                <button onClick={routeChange}>CONTINUE SHOPPING</button>
+            </div>
+
+        }
+        {orders.length >0 &&
+            <>
+            
+            </>
+        }
+      </div>
+
   )
 }
 
