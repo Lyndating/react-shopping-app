264131de70f9e8349ff82ff106fd95d0936f2a74
Ting Luo <shirleyayu@hotmail.com>
2022-05-27 20:16:33 +1000

    navigate to productshow with state data

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,10 +3,11 @@ import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { useStateValue } from '../helper/StateProvider';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 function Product({id,brand, title, image, price, rating}) {
     const [{basket}, dispatch] = useStateValue();
+    const navigate = useNavigate();
     const addToCart = () => {
         dispatch({
             type: 'add_to_basket',
@@ -21,12 +22,18 @@ function Product({id,brand, title, image, price, rating}) {
         });
     }
 
+    //redict to ProductShow Page
+    const redirectToProductShow = () => {
+        navigate(`/products/${id}`, {state:{brand: brand, title:title, image:image,price:price,rating:rating}});
+    }
+    
+
   return (
     <div className='product'>
-        <Link to={`/products/${id}`} >
+        <a onClick={redirectToProductShow} >
         <img 
             src={image} alt=''/>
-        </Link>
+        </a>
         <div className='product_info'>
             <p><strong>{brand}</strong></p>
             <p>{title}</p> 
@@ -40,7 +47,7 @@ function Product({id,brand, title, image, price, rating}) {
            </div>
         </div>
 
-            <button onClick={addToCart}>Add Item</button>
+        <button className="product_button"onClick={addToCart}>Add Item</button>
     </div>
   )
 }
