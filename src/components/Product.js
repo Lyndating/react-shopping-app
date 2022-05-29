2b3ac3d7593e2b621067777fe5a93921e3b5e4ad
Ting Luo <shirleyayu@hotmail.com>
2022-05-29 15:28:12 +1000

    pass data to productShow

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,8 +7,9 @@ import {Link, useNavigate} from 'react-router-dom';
 // import { addNewItem, getAllItems } from '../helper/storage';
 import {db} from '../firebase-config';
 
-function Product({id,brand, title, image, price, rating}) {
+function Product({id,brand, title, image, price, rating, category, data}) {
     const [{basket}, dispatch] = useStateValue();
+    console.log(data);
     const navigate = useNavigate();
     const addToCart = () => {
         dispatch({
@@ -29,30 +30,38 @@ function Product({id,brand, title, image, price, rating}) {
     // console.log("local storage", getAllItems());
     //redict to ProductShow Page
     const redirectToProductShow = () => {
-        navigate(`/products/${id}`, {state:{brand: brand, title:title, image:image,price:price,rating:rating}});
+        navigate(`/products/${id}`, {state:{brand: brand, title:title, image:image,price:price,rating:rating, category:category, data: data}});
     }
     
 
   return (
     <div className='product'>
-        <a onClick={redirectToProductShow} >
+        <a className="product_img" onClick={redirectToProductShow} >
         <img 
             src={image} alt=''/>
         </a>
         <div className='product_info'>
-            <p><strong>{brand}</strong></p>
-            <p>{title}</p> 
+            <div className='product_title'>
+                <h4><strong>{brand}</strong></h4>
+                <a onClick={redirectToProductShow}>
+                    <h5>{title}</h5> 
+                </a>
+            </div>
+            {/* <div className='product_rating'>
+                {Array(rating).fill().map((n, i)=>(<p><StarIcon/></p>))}
+            </div> */}
+        </div>
+        <div className='product_info_footer'>
             <p className='product_price'>
                 <small>$</small>
                 <strong>{price.toFixed(2)}</strong>
             </p> 
-            <div className='product_rating'>
-                {Array(rating).fill().map((n, i)=>(<p><StarIcon/></p>))}
-
-           </div>
+            <div className='product_add_to_cart'>
+                <button className="product_button"onClick={addToCart}>Add Item</button>
+            </div>
         </div>
 
-        <button className="product_button"onClick={addToCart}>Add Item</button>
+        
     </div>
   )
 }
