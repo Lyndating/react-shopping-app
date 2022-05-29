03b7f037ba50e7f910739450c8e0c72fc8101fab
Ting Luo <shirleyayu@hotmail.com>
2022-05-29 14:44:50 +1000

    add content in productshow page

--- src/components/ProductShow.js
+++ src/components/ProductShow.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { useParams, useLocation } from 'react-router-dom';
-
+import './ProductShow.css';
 
 const ProductShow = () => {
     const location = useLocation();
-
+    console.log(location.state);
     let params = useParams();
   return (
-    <div key={params.id}>
-        <p>{location.state.brand}</p>
-        <p>{location.state.title}</p>
-        <img src={location.state.image}/>
-        <p>${location.state.price}</p>
-
+    <div className='product_show_container'>
+      <div className='single_product_category_link'>
+        <p>
+          <a>Home</a>/<a>{`${location.state.category}`}</a>/<span>{`${location.state.title.toLowerCase()}`}</span>
+        </p>
+      </div>
+      <div className="sigle_product_container"key={params.id}>
+        <div className="single_product_img">
+          <img src={location.state.image}/>
+        </div>
+        <div className='single_product_info'>
+            <p>{location.state.brand}</p>
+          <p>{location.state.title}</p>
+          <p><strong>${location.state.price}</strong></p>
+        </div>
     </div>
+    </div>
+
   )
 }
 
