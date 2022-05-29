a42c0dfdae4dccc6db363284adcc2ad188bc7bec
Ting Luo <shirleyayu@hotmail.com>
2022-05-29 11:54:07 +1000

    add moment display in order list

--- src/components/SingleOrder.js
+++ src/components/SingleOrder.js
@@ -3,33 +3,45 @@ import CheckoutProduct from './CheckoutProduct';
 import "./SingleOrder.css";
 import CurrencyFormat from 'react-currency-format';
 import { subtotalAmount } from '../helper/reducer';
+import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
 
 const SingleOrder = ({id, item}) => {
-    const createdDate = new Date(item.created *1000).toISOString().split("T")[0];
-    console.log(item);
+    const createdDate = moment(item.created*1000).format("MMMM Do YYYY, h:mm:ss a");
  return (
         <div className='order_container'>
             <div className='order_time'>
-                <h3>Order created by: {createdDate}</h3>
-                <p>Order # {id}</p>
+                <h6>Order # {id}</h6>
+                <h5>Order created by: </h5>
+                <h5>{createdDate}</h5>
+                
             </div>
             <div className='item_list'>
                 {item.basket.map(product => (
-                    <CheckoutProduct
-                        id={product.id}
-                        title={product.title}
-                        image={product.image}
-                        price={product.price}
-                        qty = {product.qty}
-                    />
+                    <div className='item_list_checkout_product'>
+                        <div className='checkout_product_image'>
+                            <img src={product.image}/>
+                        </div>
+                        <div className='checkout_product_info'>
+                            <h5><strong></strong>{product.brand}</h5>
+                            <h6>{product.title}</h6>
+                            <h6>Quantity: {product.qty}</h6>
+                        </div>
+                        <div className='checkout_product_price'>
+                            <h5 className='product_price'>
+                            <small>$</small>
+                            <strong>{product.price.toFixed(2)}</strong>
+                            </h5> 
+                        </div>
+                    </div>
                 ))}
             </div>
             <div className='order_delivery_info'>
-                <p>Delivery To:</p>
+                <h5>Delivery To:</h5>
                 <div className='delivery_detail'>
-                    <p>{`${item.delivery.first_name} ${item.delivery.last_name}`}</p>
-                    <p>{item.delivery.contact}</p>
-                    <p>{item.delivery.address}</p>
+                    <h6>{`${item.delivery.first_name} ${item.delivery.last_name}`}</h6>
+                    <h6>{item.delivery.contact}</h6>
+                    <h6>{item.delivery.address}</h6>
                 </div>
             </div>
             <div className='order_amount'>
