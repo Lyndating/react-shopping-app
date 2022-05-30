bd3dab8d0e837ca70c738b57c4f7ad7388ea1149
Ting Luo <shirleyayu@hotmail.com>
2022-05-31 09:20:17 +1000

    firebase deploy

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://us-central1-shopping-react-app-deecf.cloudfunctions.net/api"
-    // 'http://localhost:5001/shopping-react-app-deecf/us-central1/api' 
+    // baseURL: 'http://localhost:5001/shopping-react-app-deecf/us-central1/api' 
     // THE API (cloud function) URL
 });
 
 export default instance;
+"https://s7ap1.scene7.com/is/image/bigw/Beauty_CT_make-up_210701?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
