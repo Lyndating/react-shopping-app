4efee3fa46acfb27fed6a395e3d9a51893e57b09
Ting Luo <shirleyayu@hotmail.com>
2022-05-30 23:32:35 +1000

    pass data to single category

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -10,42 +10,42 @@ const CategoryList = ({productCollection}) => {
                     id='nO5a1GExsyejdq3GJs8p'
                     title="Home"
                     image='https://s7ap1.scene7.com/is/image/bigw/W2487_NewSeason_CT_QuiltCovers_220224?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88'
-                    items={productCollection.Home}
+                    data={productCollection.Home}
                 />
                 <Category
                     key='fwcZLzIQxobEefZOgnk1'
                     id='fwcZLzIQxobEefZOgnk1'
                     title="Electrical"
                     image="https://s7ap1.scene7.com/is/image/bigw/Gaming%20CT-1?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
-                    items={productCollection.Electrical}
+                    data={productCollection.Electrical}
                 />
                 <Category
                     key='laPfdHI3CZzeJoj5mVki'
                     id='laPfdHI3CZzeJoj5mVki'
                     title="Beauty"
                     image="https://s7ap1.scene7.com/is/image/bigw/Beauty_CT_make-up_210701?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
-                    items={productCollection.Beauty}
+                    data={productCollection.Beauty}
                 />
                 <Category
                     key='Byrbn49hFzcoMxBHbDpH'
                     id='Byrbn49hFzcoMxBHbDpH'
                     title="Pets"
                     image="https://s7ap1.scene7.com/is/image/bigw/7406_CT_Pets_201214-new?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
-                    items={productCollection.Pets}
+                    data={productCollection.Pets}
                 />
                 <Category
-                    key='laPfdHI3CZzeJoj5mVki'
-                    id='laPfdHI3CZzeJoj5mVki'
+                    key='fUMfqRpYUzqQBdQhfzOU'
+                    id='fUMfqRpYUzqQBdQhfzOU'
                     title="Toy"
                     image="https://s7ap1.scene7.com/is/image/bigw/Home_CT_lego-conductor_210513?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
-                    items={productCollection.Toy}
+                    data={productCollection.Toy}
                 />
                 <Category
                     key='LXFKK8pDe3sL1m8GHRQj'
                     id='LXFKK8pDe3sL1m8GHRQj'
-                    title="Gift"
-                    image="https://s7ap1.scene7.com/is/image/bigw/Gifting%20CT?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
-                    items={productCollection.Gift}
+                    title="Books"
+                    image="https://s7ap1.scene7.com/is/image/bigw/1502%20Books%20CT?$cms-max-image-threshold$&fmt=webp-alpha&wid=352&fit=hfit%2C1&qlt=88"
+                    data={productCollection.Books}
                 />                
             </div>
   )
