e5bd1374a65677e851d7e287c4f2f52cbc936f24
Ting Luo <shirleyayu@hotmail.com>
2022-05-31 18:06:40 +1000

    set up env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,17 +3,17 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-o_rtbPmH_6z6qK77Er48mNxS92A3G2k",
-  authDomain: "shopping-react-app-deecf.firebaseapp.com",
-  databaseURL: "https://shopping-react-app-deecf-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "shopping-react-app-deecf",
-  storageBucket: "shopping-react-app-deecf.appspot.com",
-  messagingSenderId: "59008935241",
-  appId: "1:59008935241:web:12c8e671043308f1062698"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 
   const app = firebase.initializeApp(firebaseConfig);
 
   export const db = app.firestore();
-  export const auth = firebase.auth();
+  export const auth = app.auth();
